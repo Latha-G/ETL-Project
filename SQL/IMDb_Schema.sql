805044846cb399fdc55d3e016fb85e2001643c76
Latha-G <60633643+Latha-G@users.noreply.github.com>
2020-05-11 21:32:36 -0700

    commit code

---
+++ SQL/IMDb_Schema.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS top_imdb;
+DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS movie_actor;
+DROP TABLE IF EXISTS movie_director;
+DROP TABLE IF EXISTS streaming_service;
+DROP TABLE IF EXISTS netflix_movie;
+
+
+create table top_imdb(
+        imdb_id  INT      not null,
+    movie_title  VARCHAR  not null,
+    PRIMARY KEY  (imdb_id)
+);
+
+create table movie(
+        imdb_id  INT      not null,
+      imdb_rank  INT      not null,
+          title  VARCHAR  not null,
+           year  INT      not null,
+        runtime  INT      not null,
+          rated  VARCHAR  not null,
+    imdb_rating  INT      not null,
+    production   VARCHAR  not null,    
+    PRIMARY KEY  (imdb_id)
+    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
+);
+
+create table movie_actor(
+             id  INT      not null,
+        imdb_id  INT      not null,
+          actor  VARCHAR  not null,
+    PRIMARY KEY  (id)     
+    PRIMARY KEY  (imdb_id) 
+    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
+);
+
+
+create table movie_director(
+             id  INT      not null,
+        imdb_id  INT      not null,
+       director  VARCHAR  not null,
+    PRIMARY KEY  (id)     
+    PRIMARY KEY  (imdb_id) 
+    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
+);
+
+
+create table streaming_service(
+     service_id  INT      not null,
+   service_name  VARCHAR  not null,
+    PRIMARY KEY  (service_id) 
+);
+
+create table movie_streaming(
+            id  INT       not null,
+       imdb_id  INT       not null,
+    service_id  INT       not null,
+         price  VARCHAR   not null,
+    PRIMARY KEY (id)     
+    PRIMARY KEY (imdb_id)  
+    FOREIGN KEY (imdb_id)    REFERENCES top_imdb (imdb_id) 
+    FOREIGN KEY (service_id) REFERENCES streaming_service (service_id) 
+);
+   
+
+create table netflix_movie(
+             id  INT not null,
+        imdb_id  INT not null,
+          title  VARCHAR not null,
+    PRIMARY KEY  (id)
+    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id),
+);

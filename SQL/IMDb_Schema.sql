e8a11c76b3707040dbc63cb4c1b9d6a5cccc4767
Latha-G <60633643+Latha-G@users.noreply.github.com>
2020-05-12 19:40:30 -0700

    commit code

--- SQL/IMDb_Schema.sql
+++ SQL/IMDb_Schema.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS movie_director;
 DROP TABLE IF EXISTS streaming_service;
 DROP TABLE IF EXISTS netflix_movie;
 
-
 create table top_imdb(
         imdb_id  VARCHAR  not null,
     movie_title  VARCHAR  not null,
@@ -17,28 +16,24 @@ create table movie(
       imdb_rank  INT      not null,
           title  VARCHAR  not null,
            year  INT      not null,
-        runtime  INT      not null,
+        runtime  DECIMAL  not null,
           rated  VARCHAR  not null,
     imdb_rating  DECIMAL  not null,
     production   VARCHAR  not null,    
-    PRIMARY KEY  (imdb_id)
+    PRIMARY KEY  (imdb_id),
     FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
 );
 
 create table movie_actor(
-             id  INT      not null,
         imdb_id  VARCHAR  not null,
           actor  VARCHAR  not null,
-    PRIMARY KEY  (id,imdb_id)     
     FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
 );
 
 
 create table movie_director(
-             id  INT      not null,
         imdb_id  VARCHAR  not null,
        director  VARCHAR  not null,
-    PRIMARY KEY  (id,imdb_id)     
     FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
 );
 
@@ -49,21 +44,29 @@ create table streaming_service(
     PRIMARY KEY  (service_id) 
 );
 
-create table movie_streaming(
+create table google_movie_streaming(
             id  INT       not null,
        imdb_id  VARCHAR   not null,
     service_id  VARCHAR   not null,
          price  VARCHAR   not null,
-    PRIMARY KEY (id,imdb_id)     
-    FOREIGN KEY (imdb_id)    REFERENCES top_imdb (imdb_id) 
-    FOREIGN KEY (service_id) REFERENCES streaming_service (service_id) 
+   PRIMARY KEY  (id,imdb_id),    
+   FOREIGN KEY  (imdb_id)    REFERENCES top_imdb (imdb_id),
+   FOREIGN KEY  (service_id) REFERENCES streaming_service (service_id) 
 );
-   
+
+create table utelly_movie_streaming(
+            id  INT       not null,
+       imdb_id  VARCHAR   not null,
+    service_id  VARCHAR   not null,
+   PRIMARY KEY  (id,imdb_id),    
+   FOREIGN KEY  (imdb_id)    REFERENCES top_imdb (imdb_id),
+   FOREIGN KEY  (service_id) REFERENCES streaming_service (service_id) 
+);
 
 create table netflix_movie(
              id  INT not null,
         imdb_id  VARCHAR not null,
           title  VARCHAR not null,
-    PRIMARY KEY  (id)
-    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id),
+    PRIMARY KEY  (id),
+    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id)
 );
